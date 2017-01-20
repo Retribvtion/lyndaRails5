11d2e60f8d6239a236d0663aa1c2e4b3a9844c99
Retribvtion Rhino <sim2487@gmail.com>
2017-01-19 19:51:24 -0800

    Added Sections_controller, and built out actions for Pages and Section

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,4 +4,9 @@ class Page < ApplicationRecord
   has_and_belongs_to_many :admin_users
   has_many :sections
 
+  scope :visible, lambda { where(visible: true) }
+  scope :invisible, lambda { where(visible: false) }
+  scope :sorted, lambda { order("position ASC") }
+  scope :newest, -> { order("created_at DESC") }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-
-  get 'pages/show'
-
-  get 'pages/new'
-
-  get 'pages/edit'
-
-  get 'pages/delete'
 
   root 'demo#index'
+
+  resources :sections do
+    member do
+      get :delete
+    end
+  end
+  resources :pages do
+    member do
+      get :delete
+    end
+  end
 
   resources :subjects do
     member do
