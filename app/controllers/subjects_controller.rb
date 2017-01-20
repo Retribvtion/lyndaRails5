5ea05fe1161cd1567ffe31216d59645f7ea4387b
Retribvtion Rhino <sim2487@gmail.com>
2017-01-19 18:28:00 -0800

    Finished subjects_controller actions and generated pages controller and views

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -13,8 +13,9 @@ class SubjectsController < ApplicationController
   end
 
   def create
-    @subject = Subject.new(params[:subject])
+    @subject = Subject.new(subject_params)
     if @subject.save
+      flash[:notice] = "Subject created successfully."
       redirect_to(subjects_path)
     else
       render('new')
@@ -23,18 +24,35 @@ class SubjectsController < ApplicationController
   end
 
   def edit
-
+    @subject = Subject.find(params[:id])
   end
 
   def update
+    @subject = Subject.find(params[:id])
+    if @subject.update_attributes(subject_params)
+      flash[:notice] = "Subject updated successfully."
 
+      redirect_to(subject_path(@subject))
+    else
+      render('edit')
+    end
   end
 
   def delete
-
+    @subject = Subject.find(params[:id])
   end
 
   def destroy
+    @subject = Subject.find(params[:id])
+    @subject.destroy
+    flash[:notice] = "Subject '#{@subject.name}' destroyed successfully."
 
+    redirect_to(subjects_path)
   end
+
+  private
+    def subject_params
+      params.require(:subject).permit(:name, :position, :visible)
+    end
+
 end
